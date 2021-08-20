6cdc9741d5bb6b21a5772db82edf977328a248bb
PatelGH0512 <pateldevarshi111@gmail.com>
2021-08-21 01:42:35 +0530

    winner calculation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './components/Board';
 import './styles/root.scss';
+import { calculateWinner } from './helpers';
 
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setisXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+
+  const Message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${isXNext ? 'X' : 'O'}`;
+
+  const handlerSquareClick = position => {
+    if (board[position] || winner) {
+      return;
+    }
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+    setisXNext(prev => !prev);
+  };
+
   return (
     <div className="app">
       <h1>TicTacToe App</h1>
-      <Board />
+      <h2> {Message}</h2>
+      <Board board={board} handlerSquareClick={handlerSquareClick} />
     </div>
   );
 };
